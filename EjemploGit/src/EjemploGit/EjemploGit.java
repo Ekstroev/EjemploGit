d1e7acabb6e93c4b1b9edea8da4f4d5f35389c48
Ekaterina <Ekaterina@192.168.1.140>
2021-01-21 09:01:14 +0100

    Mensaje de commit

---
+++ EjemploGit/src/EjemploGit/EjemploGit.java
@@ -0,0 +1,13 @@
+package EjemploGit;
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 25; i > 0; i --) {
+			System.out.println("Hola mundo");
+		}
+
+	}
+
+}
